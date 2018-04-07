365aad3ef409698830bb4c1f73a0d07c74600975
Joshenberg <jfrothen@uvm.edu>
2018-04-07 18:31:35 -0400

    Main class to use as a backup for the liberty/java tool that wouldn't function

---
+++ src/main/java/application/main.java
@@ -0,0 +1,14 @@
+package application;
+
+public class main {
+
+    public static void main(String[] args) {
+        boolean running = true;
+        boolean userExists = false;
+
+        while (running) {
+            
+        }
+    }
+
+}
