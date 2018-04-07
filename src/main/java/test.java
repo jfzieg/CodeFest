503211c482d2e0c45691948e26cba3fd145621db
agrech4 <alexiv42@gmail.com>
2018-04-07 11:28:57 -0400

    added class object

---
+++ src/main/java/test.java
@@ -0,0 +1,15 @@
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
+
+public class test{
+	public static void main(String[] args) {
+		NaturalLanguageUnderstanding NLUservice = new NaturalLanguageUnderstanding(
+				  "2018-03-16",
+				  "3ef0284a-d3e1-43a5-bf2e-86e270c07996",
+				  "6dhn05xkaU2z"
+				);
+		String courseDescription = "You will learn about the Object-Oriented Programming paradigm and gain knowledge of classes,\r\n" + 
+				"encapsulation, polymorphism, inheritance, and virtual classes. You will use the C++\r\n" + 
+				"programming language to design efficient, sustainable projects.";
+		Class.getTags(NLUservice, courseDescription);
+	}
+}
