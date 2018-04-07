428d2c6c15aae82d744cd057e694e5332cd45b2a
Austin R Batistoni <austin.batistoni@uvm.edu>
2018-04-07 15:24:55 -0400

    Created jobs api and tests

---
+++ src/main/java/wasdev/sample/rest/GitJobsApi.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.io.*;
+import java.net.*;
+
+import org.json.*;
+
+public class GitJobsApi{
+	public GitJobsApi() {
+		
+	}
+	
+	/**
+	 * This method reads all lines from the HTTP Get request
+	 * @Param rd: bufferedreader object 
+	 * @return whole response as string
+	 */
+	public static String readAll(BufferedReader rd) throws IOException {
+	    	StringBuilder sb = new StringBuilder();
+	    	int cp;
+	    	String line;
+		      while ((line = rd.readLine()) != null) {
+		         sb.append(line);
+		      }
+		      rd.close();
+	    	return sb.toString();
+	  }
+
+	
+	/**
+	 * This method reads content from HTTP Get request and converts it to a JSONArray (array of JSON objects)
+	 * @param url: the url of the get request
+	 * @return Whole response in the form of a JSONArray 
+	 */
+	public static JSONArray readJsonFromUrl(String url) throws IOException, JSONException {
+		  InputStream is = new URL(url).openStream();
+		  try {
+			  BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			  String jsonText = readAll(rd);
+			  //jsonText = jsonText.replace("[", "").replace("]", "");
+			  JSONArray json = new JSONArray(jsonText);
+			  return json;
+	   		} finally {
+	   			is.close();
+	   		}
+	  }
+   	
+}
--- src/test/java/test/MyTest.java
+++ src/test/java/test/MyTest.java
@@ -1,7 +1,25 @@
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
+import junit.framework.Assert;
+import org.junit.Test;
+import org.json.*;
+import java.io.IOException;
 
-public class test{
-	public static void main(String[] args) {
+
+
+public class MyTest{
+	@Test
+	public void test1() throws IOException{
+		
+		GitJobsApi a = new GitJobsApi();
+		JSONArray json = a.readJsonFromUrl("https://jobs.github.com/positions.json?description=java"); 
+		JSONObject b = json.getJSONObject(0); //Indexes first result of the JSONArray
+		System.out.println(b.get("description")); //Prints just the value associated with the description key
+	    
+	}
+	
+	
+	@Test
+	public void test2() {
 		NaturalLanguageUnderstanding NLUservice = new NaturalLanguageUnderstanding(
 				  "2018-03-16",
 				  "3ef0284a-d3e1-43a5-bf2e-86e270c07996",
@@ -11,5 +29,6 @@ public class test{
 				"encapsulation, polymorphism, inheritance, and virtual classes. You will use the C++\r\n" + 
 				"programming language to design efficient, sustainable projects.";
 		Class.getTags(NLUservice, courseDescription);
+		
 	}
 }
