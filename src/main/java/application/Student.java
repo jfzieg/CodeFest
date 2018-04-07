c544ce3b106302c0f7c8de98a6073539cb794da6
Joshenberg <jfrothen@uvm.edu>
2018-04-07 14:40:24 -0400

    Changed all lists to arrayLists Creating functions for ranking one's fields

--- src/main/java/application/Student.java
+++ src/main/java/application/Student.java
@@ -1,20 +1,21 @@
 package application;
 
-import java.util.List;
+import java.util.*;
 
 public class Student {
 	private String name = null;
-	private List<String> major;
-	private List<String> minor;
+	private ArrayList<String> major;
+	private ArrayList<String> minor;
 	
-	private List<String> fields;
-	private List<Course> courses;
+	private ArrayList<String> fields;
+	private ArrayList<Integer> fieldRanks;
+	private ArrayList<Course> courses;
 	
 	public Student() {
 		this.name = "";
 	}
 	
-	public Student(String s, List<String> majors, List<String> minors) {
+	public Student(String s, ArrayList<String> majors, ArrayList<String> minors) {
 		this.name = s;
 		this.major = majors;
 		this.minor = minors;
@@ -26,7 +27,7 @@ public class Student {
 	
 	//Allows input of majors delimited by comma and space, like "Computer Science, Math"
 	public void enterMajors(String s) {
-		major = new List<String>;
+		major = new ArrayList<String>();
 		String[] majorArray = s.split(", ");
 		for (String ss : majorArray) {
 			major.add(ss);
@@ -34,7 +35,7 @@ public class Student {
 	}
 	
 	public void enterMinors(String s) {
-		minor = new List<String>;
+		minor = new ArrayList<String>();
 		String[] minorArray = s.split(", ");
 		for (String ss : minorArray) {
 			minor.add(ss);
@@ -43,12 +44,51 @@ public class Student {
 	
 	public void addCourse(Course c){
 		courses.add(c);
+		String field = c.getField();
+		if (fields.contains(field)){
+		    fieldRanks.get( fields.indexOf(field) ) += 1;
+        }
+        else {
+		    fields.add(c.getField());
+		    fieldRanks.
+        }
+
 	}
 	
 	public void setName(String visitorName) {
 		this.name = visitorName;
 	}
-	
+
+	public ArrayList<String> deleteDuplicateFields(ArrayList<String> longList) {
+	    Set<String> deleter = new HashSet<>();
+	    deleter.addAll(longList);
+	    ArrayList<String> shortList = new ArrayList<>();
+	    shortList.addAll(deleter);
+	    return shortList;
+    }
+
+	public void rankFields() {
+
+	    int[] fieldRanking = new int[fields.size()];
+
+	    for (int i = 0; i < fields.size(); i++) {
+	        for (String s2 : fields) {
+	            if (fields.get(i).equalsIgnoreCase(s2)){
+	                fieldRanking[i]++;
+                }
+            }
+        }
+
+        int max = 0;
+	    int highestIndex = -1;
+        for (int rank : fieldRanking) {
+	        if (rank > max) {
+	            max = rank;
+	            highestIndex =
+            }
+        }
+
+    }
 	
 
 }
